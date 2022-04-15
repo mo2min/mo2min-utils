e57b4ecea4bdfc9818d34097312ae416355234c6
mo2min <fireb1003@googlemail.com>
2022-04-15 22:10:42 +0000

    getTextSummary

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export default function sayWhat(what: string) {
+export function sayWhat(what: string) {
   console.log("Say ," + what);
+}
+
+export function getTextSummary(text: string, words?: number) {
+  return text
+    .substring(text.indexOf('\n') + 1)
+    .split(' ')
+    .slice(0, words ? words : 30)
+    .join(' ');
 }
